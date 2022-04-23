2985117f89aa43bd832118a16ef909d46dfe73a1
sergei <zolotenkov999@mail.ru>
2022-04-23 06:55:53 +0300

    сам написал код (имя и фомилия)1 ,выполнил сложение строк, написал код,написал javascript,написал "hello world",перешол к длине строки

--- script3.js
+++ script3.js
@@ -3,19 +3,27 @@
 комментарий
 */
 alert('3 страниц');
-let num;    // объявим переменную
-num = 100;    // присвоим ей значение
-alert(num); // выведем значение переменной на экран
-let a, b; //объявляем 2 переменные
-a=10;
-b=5;
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a/b);
-let c, d, result;
-c=(a-b);
-d=7;
-result=(c+d);
-alert(result);
-alert(a+b+c);
+// let num;    // объявим переменную
+// num =- 100;    // присвоим ей значение
+// alert(num); // выведем значение переменной на экран
+// let a, b; //объявляем 2 переменные
+// a=10;
+// b=5;
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// alert(a/b);
+// let c, d, result;
+// c=(a-b);
+// d=7;
+// result=(c+d);
+// alert(result);
+// alert(a+b+c);
+// let i, f;
+// i = "сергей";
+// f = "золотенков";
+// alert(i+f);
+let str, rst;
+str="hello"
+rst="world"
+alert(str+ " "+rst);
