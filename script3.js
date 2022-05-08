6922252a829e7271ea3bd810ec115777c0acae6e
sergei <ulazolote@g.mail.cjm>
2022-05-08 14:03:15 +0300

    Строки с цифрами в js. Задача 1 сумма строки '12345' (не понял ?)

--- script3.js
+++ script3.js
@@ -88,9 +88,8 @@
 // let str='abcde';
 // let num = 2;
 // alert(str[num]);
-let str='javascript';
-alert(str [str.length -3]);
-
+let a='12345';
+alert(Number(a[0] + Number(a[2] + Number(a[3] + Number(a[4])))));
 
 
 
