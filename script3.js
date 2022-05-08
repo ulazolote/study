eb34515824be07f4af3ea6eff9227b924c7b0f25
sergei <ulazolote@g.mail.cjm>
2022-05-08 14:20:27 +0300

    Обращение к цифрам числа. Задача 2 нашел сумму числа 12345, =15.

--- script3.js
+++ script3.js
@@ -92,8 +92,8 @@
 // alert(Number(a[0]) + Number(a[2]) + Number(a[3]) + Number(a[4]));..
 // let a='12345';
 // alert(Number(a[0])+Number(a[1])+Number(a[2])+Number(a[3])+Number(a[4]));
-
-
+let a=String(12345);
+alert(Number(a[0])+Number(a[1])+Number(a[2])+Number(a[3])+Number(a[4]));
 
 
 
