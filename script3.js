9ece5b9720b9ee408d20194522eaf40f39a3afad
sergei <ulazolote@g.mail.cjm>
2022-05-21 10:29:07 +0300

    Заполнение массива. Задача 11 заполнил массив. Задача 12 добавил к массиву 4 и 5.

--- script3.js
+++ script3.js
@@ -362,3 +362,15 @@
 // arr[2]++;
 // console.log(arr);
 
+//задача 11 заполнение массива
+// let arr=[];
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// console.log(arr);
+
+//задача 12 4и5 в конце
+let arr=[1,2,3];
+arr[3]=4;
+arr[4]=5;
+console.log(arr);
