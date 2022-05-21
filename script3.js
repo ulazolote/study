c1bcad7a5a17a871cf906d1b886f1be9fd162646
sergei <ulazolote@g.mail.cjm>
2022-05-21 10:41:41 +0300

    Разреженный массив. Задача 13 длина массива (length) = 9

--- script3.js
+++ script3.js
@@ -370,7 +370,14 @@
 // console.log(arr);
 
 //задача 12 4и5 в конце
-let arr=[1,2,3];
-arr[3]=4;
-arr[4]=5;
-console.log(arr);
+// let arr=[1,2,3];
+// arr[3]=4;
+// arr[4]=5;
+// console.log(arr);
+
+//задача 13 длина массива (length)
+// let arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+// console.log(arr.length);// выведет 9
+
