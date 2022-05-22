771e2afec41b6956bd45633078570f7e488f5ec1
sergei <ulazolote@g.mail.cjm>
2022-05-22 09:02:23 +0300

    Ключи из переменных в JS. Задача 1 Вывел на экран элемент, ключ которого хранится в переменной key. Задача 2 Нашел сумму элементов, ключи которых хранятся в наших переменных.

--- script3.js
+++ script3.js
@@ -414,8 +414,22 @@
 // console.log(date.year+'-'+date.month+'-'+date.day);
 
 //задача 9 создание объекта
-let obj={};
-obj.a='1';
-obj.b='2';
-obj.c='3';
-console.log(obj);
+// let obj={};
+// obj.a='1';
+// obj.b='2';
+// obj.c='3';
+// console.log(obj);
+
+//ключи из переменных
+//задача 1 ключ в key
+// let arr = ['a', 'b', 'c'];
+// let key = 2;
+// console.log(arr[key]);
+
+//задача 2 сумма
+// let arr=[1,2,3,4,5];
+// let key1=1;
+// let key2=2;
+// console.log(arr[key1]+arr[key2]);
+
+//
