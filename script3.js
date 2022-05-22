a014abfeff4236a8d0b1703db8d99b99eb0ae182
sergei <ulazolote@gmail.com>
2022-05-22 10:28:13 +0300

    Передача объектов по ссылке в JS. Задача 1 выведет 'a', 2, 3 . Задача 2 выведет 'a', 'b', 3 . Задача 3 выведет 'b', 2, 3.

--- script3.js
+++ script3.js
@@ -456,4 +456,25 @@
 //задача 7
 // console.log( Array.isArray([1, 2, 3]) );
 //задача 8
-console.log( Array.isArray({a: 1, b: 2, c: 3}) );
+// console.log( Array.isArray({a: 1, b: 2, c: 3}) );
+
+//передача объектов по ссылке
+//задача 1
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// console.log(arr2);
+
+//задача 2
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[1] = 'b';
+// console.log(arr1);
+
+//задача 3
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr1[0] = 'a';
+arr2[0] = 'b';
+console.log(arr2);
