76573d17f75f1384098f2795bf3f334dbb963b65
sergei <ulazolote@gmail.com>
2022-05-22 10:32:40 +0300

    ОПЕРАТОР in в JS. Задача 1 выведет true, false.

--- script3.js
+++ script3.js
@@ -473,8 +473,14 @@
 // console.log(arr1);
 
 //задача 3
-let arr1 = [1, 2, 3];
-let arr2 = arr1;
-arr1[0] = 'a';
-arr2[0] = 'b';
-console.log(arr2);
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr1[0] = 'a';
+// arr2[0] = 'b';
+// console.log(arr2);
+
+//оператор in
+//задача 1
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+console.log('e' in obj);
+console.log('f' in obj);
