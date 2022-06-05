0f8512281d2e4f51206e581a03dae1055cc5db50
sergei <ulazolote@gmail.com>
2022-06-05 07:51:57 +0300

    Область видимости let и var в if-else. Область видимости var. Задача 6 выведет верно. Задача 7 выведет undefined.

--- script3.js
+++ script3.js
@@ -965,16 +965,18 @@
 // console.log(result);
 
 //Задача 5
-let age = 19;
-let result;
-if (age >= 18) {
-     if (age <= 23) {
-          result = 'от 18 до 23';
-     } else {
-          result = 'больше 23';
-     }
-} else {
-     result = 'меньше 18';
-}
-console.log(result);
+// let age = 19;
+// let result;
+// if (age >= 18) {
+//      if (age <= 23) {
+//           result = 'от 18 до 23';
+//      } else {
+//           result = 'больше 23';
+//      }
+// } else {
+//      result = 'меньше 18';
+// }
+// console.log(result);
+
+
 
